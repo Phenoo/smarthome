e7ccdf50940bafe2440d7519592b3a2873deff97
Phenoo <descometusah@gmail.com>
2024-09-16 22:17:27 +0100

    Initial Commit

---
+++ SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Eze Chidera Paschal on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
